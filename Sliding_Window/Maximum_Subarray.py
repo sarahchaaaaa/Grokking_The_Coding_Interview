788e02004e71471268d2bbd491bf1741e0e8552d
sarahchaaaaa <36656826+sarahchaaaaa@users.noreply.github.com>
2022-04-05 16:28:07 -0500

    Update Maximum_Subarray.py

--- Sliding_Window/Maximum_Subarray.py
+++ Sliding_Window/Maximum_Subarray.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currSubarray = nums[0]
+        maxTotal = nums[0]
+        
+        for num in nums[1:]:
+            currSubarray = max(num, currSubarray + num)
+            maxTotal = max(maxTotal, currSubarray)
+        return maxTotal
